9bb53d8de6860f2d92bd77666885f7d6a0b91999
abubjazov <abubjazov@mail.ru>
2022-02-18 20:39:09 +0300

    App.test: Fire Events

--- src/App.test.js
+++ src/App.test.js
@@ -1,38 +1,26 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import { App } from './App'
 
 describe('App', () => {
   it('renders App component', async () => {
     render(<App />)
-    expect(screen.queryByText(/Logged in as/)).toBeNull()
-    expect(await screen.findByText(/Logged in as/)).toBeInTheDocument()
-    expect(screen.getByAltText(/search/)).toHaveClass("image")
-    expect(screen.getByLabelText(/search/i)).not.toBeRequired()
-    expect(screen.getByLabelText(/search/i)).toBeEmptyDOMElement()
-    expect(screen.getByLabelText(/search/i)).toHaveAttribute("id")
+    await screen.findByText(/logged in as/i)
+    expect(screen.queryByText(/searches for react/i)).toBeNull()
+    fireEvent.change(screen.getByRole('textbox'), {
+      target: { value: 'React' }
+    })
+    expect(screen.getByText(/searches for react/i)).toBeInTheDocument()
   })
 })
 
-/*
-Search variants:
-  getBy:                    queryby:                    findBy:
-- getByText               - queryByText               - findByText
-- getByRole               - queryByRole               - findByRole
-- getByLabelText          - queryByLabelText          - findByLabelText
-- getByPlaceholderText    - queryByPlaceholderText    - findByPlaceholderText
-- getByAltText            - queryByAltText            - findByAltText
-- getByDisplayValue       - queryByDisplayValue       - findByDisplayValue
-- getAllBy                - queryAllBy                - findAllBy
-*/
-
-/*
-Assertive Functions:
-- toBeDisabled            - toBeEnabled               - toBeEmpty
-- toBeEmptyDOMElement     - toBeInTheDocument         - toBeInvalid
-- toBeRequired            - toBeValid                 - toBeVisible
-- toContainElement        - toContainHTML             - toHaveAttribute
-- toHaveClass             - toHaveFocus               - toHaveFormValues
-- toHaveStyle             - toHaveTextContent         - toHaveValue
-- toHaveDisplayValue      - toBeChecked               - toBePartiallyChecked
-- toHaveDescription
-*/
+describe('events', () => {
+  it('checkbox click', () => {
+    const handleChange = jest.fn()
+    const { container } = render(<input type={'checkbox'} onChange={handleChange}/>)
+    const checkbox = container.firstChild
+    expect(checkbox).not.toBeChecked()
+    fireEvent.click(checkbox)
+    expect(handleChange).toHaveBeenCalledTimes(1)
+    expect(checkbox).toBeChecked()
+  })
+})
